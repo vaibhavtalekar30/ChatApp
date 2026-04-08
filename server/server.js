5872fe1d2c8d4b12a559bc30714cca21eb0d3b24
vaibhavtalekar30 <vaibhav.n.talekar885@gmail.com>
2026-04-08 12:42:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ const io = new Server(server, {
   }
 });
 
-initializeSocket(io);
+//initializeSocket(io);
 
 server.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
