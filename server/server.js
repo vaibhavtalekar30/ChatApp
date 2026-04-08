a666b354e5f884b2ebaef4009c8f5676b59817f1
vaibhavtalekar30 <vaibhav.n.talekar885@gmail.com>
2026-04-08 12:09:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(errorHandler);
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: ["http://localhost:5173"] ,
+  cors: { origin: ["https://chat-app-5lx4.vercel.app"] ,
   methods: ["GET", "POST"],
   credentials: true,
   }
