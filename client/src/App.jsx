88dae387b5364d2f6c44f0655f1c0c1630d6ba4e
vaibhavtalekar30 <vaibhav.n.talekar885@gmail.com>
2026-04-08 11:36:58 +0530

    Update import paths for Login, Register, and Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
 //import ChatPage from "./pages/ChatPage";
 import { AuthProvider } from "./context/AuthContext";
 import { useAuth } from "./context/AuthContext";
